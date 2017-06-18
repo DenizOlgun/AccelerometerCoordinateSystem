049c32179d5efdd885da4241fb150a0969e1e396
DenizOlgun <dg.olgun@gmail.com>
2017-06-17 20:00:03 -0400

    6/17/17

---
+++ src/CoordinateSystemBase.java
@@ -0,0 +1,89 @@
+import java.util.function.Function;
+
+import static java.lang.Math.*;
+
+public class CoordinateSystemBase {
+
+    private static double x = 0; //x+ right, x- left
+    private static double y = 0; //y+ up, y- down
+
+
+    public static void incrementX(double incrementVal) {
+
+        x += incrementVal;
+    }
+
+    public static void incrementY(double incrementVal) {
+
+        y += incrementVal;
+    }
+
+    private static double startTime; //seconds
+
+    static final double timeBetweenReadings = 0.05D;
+
+    static final double timeBetweenReadingsSquared = pow(timeBetweenReadings, 2);
+
+    public static double elapsedTime() { //seconds
+
+        return (System.currentTimeMillis()/1000D) - startTime;
+    }
+
+    public static void delayUntil(double delayAmt) { //delays for `delayAmt` seconds after the program start
+
+        while (elapsedTime() < delayAmt) ;
+    }
+
+    public static double[] calculateMove(double angle, Function<Double, Double> acceleration, double totalTime) {
+
+
+        double sum = 0;
+        double accelerationResult;
+        double benchmark = timeBetweenReadings; //initial benchmark
+        Function<Double, Double> timeForNextReading = (a) -> {
+
+            double foo = elapsedTime() / timeBetweenReadings;
+            if(rint(foo) == foo) return timeBetweenReadings * (foo + 1);
+            else return timeBetweenReadings * ceil(foo);
+        };
+
+        startTime = System.currentTimeMillis()/1000D;
+        while (elapsedTime() < totalTime) {
+
+            //if(elapsedTime() > benchmark) throw new RuntimeException("Benchmark was passed due to small margins; increase the value of TIME_BETWEEN_READINGS" + " currentTime: " + elapsedTime() + " benchmark: " + benchmark); //this line causes first number to be skipped
+            accelerationResult = acceleration.apply(elapsedTime());
+            benchmark = timeForNextReading.apply(-1D);
+            sum += accelerationResult; // * TIME_BETWEEN_READINGS; should be multiplied with TIME_BETWEEN_READINGS, but it's faster to multiply with the squared version, so it helps not miss the benchmark
+            incrementX(sum * timeBetweenReadingsSquared * Math.sin(angle * PI/180D));
+            incrementY(sum * timeBetweenReadingsSquared * Math.cos(angle * PI/180D));
+            delayUntil(benchmark);
+        }
+
+        System.out.printf("%f\n", elapsedTime());
+        System.out.println();
+        System.out.println("Calculated x: " + x);
+        System.out.println("Calculated y: " + y);
+        System.out.println();
+        System.out.println("Calculated magnitude: " + sqrt(x*x + y*y));
+        System.out.println();
+        System.out.println("Time between each reading: " + timeBetweenReadings);
+        System.out.println("Fixed angle: " + angle + " degrees");
+        System.out.println("Final Velocity: " + sum * timeBetweenReadings);
+
+        double[] result = new double[2];
+        //double correctionFactor = 100D/101;
+        result[0] = x;// * 100D / 101D;
+        result[1] = y;// * 100D / 101D;
+        return result;
+    }
+
+    public static void reset() { //see the reset() comment in Tests
+
+        x = 0;
+        y = 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Tests.java
@@ -0,0 +1,173 @@
+import org.junit.*;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.*;
+
+import static java.lang.Math.*;
+
+/**
+ * Created by 4924_Users on 6/10/2017.
+ */
+public class Tests  {
+
+    double startAngle;
+    Function<Double, Double> acceleration;
+    double totalTime;
+    double[] expectedResult = new double[2];
+    double[] actualResult;
+    final double DELTA = 0.000001;
+    final double X_ANGLE = 90; //y = 0
+    final double Y_ANGLE = 0; //x = 0
+    final double XY_ANGLE = 90 - atan(2)*180/PI; //y = 2x
+
+    @Before
+    public void reset() { //currentTime is now reset in CoordinateSystemBase, initialization doesn't mess up the timing, but apparently the exception check takes more than 0.05 seconds to start
+
+        CoordinateSystemBase.reset();
+    }
+
+    @Test
+    public void noAccelerationXTest() {
+
+        startAngle = X_ANGLE;
+        acceleration = t -> 0D;
+        totalTime = 5D;
+        expectedResult[0] = 0;
+        expectedResult[1] = 0;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void constantAccelerationXTest() { //consistently 1% too large
+
+        startAngle = X_ANGLE;
+        acceleration = t -> 2D;
+        totalTime = 5D;
+        expectedResult[0] = acceleration.apply(totalTime) * pow(totalTime, 2) / 2D;
+        expectedResult[1] = 0;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void noAccelerationYTest() {
+
+        startAngle = Y_ANGLE;
+        acceleration = t -> 0D;
+        totalTime = 5D;
+        expectedResult[0] = 0;
+        expectedResult[1] = 0;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void constantAccelerationYTest() { //consistently 1% too large
+
+        startAngle = Y_ANGLE;
+        acceleration = t -> 2D;
+        totalTime = 5D;
+        expectedResult[0] = 0;
+        expectedResult[1] = acceleration.apply(totalTime) * pow(totalTime, 2) / 2D;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void constantAccelerationXYTest() {
+
+        startAngle = XY_ANGLE;
+        acceleration = t -> 2D;
+        totalTime = 5D;
+        expectedResult[0] = acceleration.apply(totalTime) * pow(totalTime, 2) / (2D * sqrt(5));
+        expectedResult[1] = expectedResult[0] * 2;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void noAccelerationXYTest() {
+
+        startAngle = XY_ANGLE;
+        acceleration = t -> 0D;
+        totalTime = 5D;
+        expectedResult[0] = 0;
+        expectedResult[1] = 0;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void linearAccelerationXTest() {
+
+        startAngle = X_ANGLE;
+        acceleration = t -> 2*t;
+        totalTime = 5D;
+        expectedResult[0] = acceleration.apply(1D) * pow(totalTime, 3) / 6D;
+        expectedResult[1] = 0;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void linearAccelerationYTest() {
+
+        startAngle = Y_ANGLE;
+        acceleration = t -> 2*t;
+        totalTime = 5D;
+        expectedResult[0] = 0;
+        expectedResult[1] = acceleration.apply(1D) * pow(totalTime, 3) / 6D;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void linearAccelerationXYTest() {
+
+        startAngle = XY_ANGLE;
+        acceleration = t -> 2*t;
+        totalTime = 5D;
+        expectedResult[0] = acceleration.apply(1D) * pow(totalTime, 3) / (6D * sqrt(5));
+        expectedResult[1] = expectedResult[0] * 2;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void sinusoidalAccelerationXTest() {
+
+        startAngle = X_ANGLE;
+        acceleration = t -> 2*sin(t);
+        totalTime = 5D;
+        expectedResult[0] = (acceleration.apply(totalTime) * -1D) + (totalTime * acceleration.apply(PI/2)); //the addition of totalTime accounts for the arbitrary constant
+        expectedResult[1] = 0;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void sinusoidalAccelerationYTest() {
+
+        startAngle = Y_ANGLE;
+        acceleration = t -> 2*sin(t);
+        totalTime = 5D;
+        expectedResult[0] = 0;
+        expectedResult[1] = (acceleration.apply(totalTime) * -1D) + (totalTime * acceleration.apply(PI/2)); //the addition of totalTime accounts for the arbitrary constant
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void sinusoidalAccelerationXYTest() {
+
+        startAngle = XY_ANGLE;
+        acceleration = t -> 2*sin(t);
+        totalTime = 5D;
+        expectedResult[0] = (acceleration.apply(totalTime) * -1D + totalTime * acceleration.apply(PI/2)) / sqrt(5);
+        expectedResult[1] = expectedResult[0] * 2;
+        actualResult = CoordinateSystemBase.calculateMove(startAngle, acceleration, totalTime);
+        assertArrayEquals(expectedResult, actualResult, DELTA);
+    }
+}
